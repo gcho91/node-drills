2c800a7712c5bec394776ae7b7ac1d1d9928f5b3
gcho91 <gcho91@gmail.com>
2017-09-26 00:15:04 -0500

    Almost done with App5, needs review and help

--- app5-queries_params/index.js
+++ app5-queries_params/index.js
@@ -7,6 +7,52 @@ var app = express();
 
 app.use(bodyParser.json());
 
+// app.get('/data', function(req, res, next) {
+// 	// console.log(data);
+// 	res.status(200).json(data);
+// });
+
+//vvvvvvvWHATTTTT?????VVVVV//
+app.get('/data', function(req, res, next) {
+	var query = Object.keys(req.query)[0];
+	data = req.query[query] ? data.filter(function(elem) {
+		return elem[query] === req.query[query]
+	}) : data;
+	res.status(200).json(data);
+})
+
+
+//create an endpoint to get one item by index/id
+//use req.params
+
+app.get('/data/:id', function(req, res, next) {
+	console.log('params :', req.params.id)
+	res.status(200).json(data[req.params.id])
+
+})
+
+//below example of how to do params if not indexing//
+// app.get('/data/:sex', function(req, res, next) {
+// 	// console.log('params :', req.params.id)
+// 	res.status(200).json(data.filter(x => x.sex === req.params.sex ))
+// })
+
+
+
+app.post('/job', function(req, res, next) {
+	// console.log(req.body);
+	data.push(req.body);
+	res.status(200).json(data);
+});
+
+
+
+
+app.delete('/data/:id', function(req, res, next) {
+	// console.log(req.body);
+	data.splice(req.params.id, 1),
+	res.status(200).json(data);
+})
 
 
 
